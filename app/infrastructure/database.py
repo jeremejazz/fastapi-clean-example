5eab525a444eb981a35e690f6350f94ff8219972
Jereme Causing <jeremejazz15@gmail.com>
2025-12-26 23:18:46 +0800

    fix outdated orm API

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -1,20 +1,20 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+Engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
+SessionLocal = sessionmaker(autoflush=False, bind=Engine)
 
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
+class Base(DeclarativeBase):
+    pass
 
-Base = declarative_base()
-
-def get_db():
+def Get_db():
     db = SessionLocal()
     try:
         yield db
--- app/infrastructure/orm.py
+++ app/infrastructure/orm.py
@@ -1,24 +1,23 @@
 
 from sqlalchemy import Column, ForeignKey, Integer, Numeric, String
-from sqlalchemy.orm import relationship, MappedAsDataclass, Mapped, mapped_column
+from sqlalchemy.orm import relationship, mapped_column
 
 from app.infrastructure.database import Base
 
 
-class CategoryORM(MappedAsDataclass, Base):
+class CategoryORM(Base):
     __tablename__ = "categories"
-    id: Mapped[int] = mapped_column(primary_key=True, init=False)
-    name: Mapped[str]
-    # id = Column(Integer, primary_key=True, index=True)
-    # name = Column(String, unique=True, index=True)
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
                   
 
 class ProductORM(Base):
-    __tablenamne__ = "products"
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    price = Column(Numeric(10, 2, as_decimal=True))
+    price = Column(Numeric(precision=10, scale=2))
     category_id = Column(Integer, ForeignKey("categories.id"))
 
     category = relationship("CategoryORM", back_populates="products")
